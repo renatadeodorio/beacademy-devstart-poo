bc291bae359889ad32b723cffccd634078028a1e
= <=>
2022-06-13 17:21:10 -0300

    Ajustes aula01

--- modulo02/aula01/index.php
+++ modulo02/aula01/index.php
@@ -9,7 +9,10 @@ include "Professor.php";
 $p1 = new Professor();
 $p1->nome = "Renata";
 $p1 ->cpf ="123.123.123-12";
-$p1-> salario = 1000 ;
+$p1-> salario = 1000 ;  
+
+
+$p1-> cpf ="RT12GH456";
 
 var_dump($p1);
 
